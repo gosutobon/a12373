3f2a56b81d304ceb8cea3144005fb1ef5a6aeaa8
gosutobon <vincent40866@gmail.com>
2024-08-23 10:43:50 +0800

    第一次成功

---
+++ a12345/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace a12345
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("123");
+        }
+    }
+}
